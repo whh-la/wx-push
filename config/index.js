6a388ddbe41ad715ba50a45f65df0e4fc72c911e
whh-la <1505965829@qq.com>
2024-09-24 11:51:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx802ec0e3664e1afe",
   appsecret: "b3b4753887ccb6f2b8ce489fdab5ca0d",
   template_id: "vqOEnuwdQ0UxvtJpb_GLysNT8bp4103wahSz2ocBJ-A",
-  touser: "oJwij6bXLXLzIT2_XLKLSTcdw7sk",
+  touser: "oJwij6bXLXLzIT2_XLKLSTcdw7sk,oJwij6Taqa1yOLcL9IOt0kXQMO1E",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "天津",
